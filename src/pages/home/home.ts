b060b1b7c6c709030adba57da6fa2c8bae3f91b3
Philip <Philip.zdb@gmail.com>
2017-03-05 16:04:40 +0100

    Implements sharing functionality in home controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+
+import {SocialSharing} from 'ionic-native'
 
 @Component({
   selector: 'page-home',
@@ -9,7 +11,21 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    
+
   }
+
+  whatsappShare() {
+    SocialSharing.shareViaWhatsApp("Message Via WhatsApp", null /*Image*/, "https://medium.com/@philip.mzdb")
+  }
+  twitterShare() {
+    SocialSharing.shareViaTwitter("Message Via Twitter", null /*Image*/, "https://medium.com/@philip.mzdb")
+  }
+  facebookShare() {
+    SocialSharing.shareViaFacebook("Message Via Facebook", null /*Image*/, "https://medium.com/@philip.mzdb")
+  }
+  otherShare() {
+    SocialSharing.share("General Share Sheet", null /*Image*/, "https://medium.com/@philip.mzdb")
+  }
+
 
 }
